a2f374cc33b5ccb0dd67ada62cebcc9859f4a5c9
VeryCreativeName64 <marekbendeguz@gmail.com>
2024-11-20 11:08:34 +0100

    Ellenőrzi a helyes felhasználást a program

--- src/main/KoPapirOllo.java
+++ src/main/KoPapirOllo.java
@@ -11,27 +11,25 @@ public class KoPapirOllo {
         String[] opciok = {"Kő", "Papír", "Olló"};
         Random random = new Random();
 
-        
         System.out.println("Kérlek, válassz: Kő (0), Papír (1), Olló (2):");
         int jatekosValasztas = sc.nextInt();
-        
-        if (jatekosValasztas < 0 || jatekosValasztas > 2) {
-        System.out.println("Helytelen választás! Csak 0, 1 vagy 2 lehet.");
-        return;
+
+        while (jatekosValasztas < 0 || jatekosValasztas > 2) {
+            System.out.println("Helytelen választás! Csak 0, 1 vagy 2 lehet.");
+            System.out.println("Kérlek, válassz: Kő (0), Papír (1), Olló (2):");
+            jatekosValasztas = sc.nextInt();
         }
-       
+        
         int gepValasztas = random.nextInt(3);
 
-        
         System.out.println("Játékos választása: " + opciok[jatekosValasztas]);
         System.out.println("Gép választása: " + opciok[gepValasztas]);
 
-        
         if (jatekosValasztas == gepValasztas) {
             System.out.println("Döntetlen!");
-        } else if ((jatekosValasztas == 0 && gepValasztas == 2) ||
-                   (jatekosValasztas == 1 && gepValasztas == 0) ||
-                   (jatekosValasztas == 2 && gepValasztas == 1)) {
+        } else if ((jatekosValasztas == 0 && gepValasztas == 2)
+                || (jatekosValasztas == 1 && gepValasztas == 0)
+                || (jatekosValasztas == 2 && gepValasztas == 1)) {
             System.out.println("Játékos nyert!");
         } else {
             System.out.println("Gép nyert!");
